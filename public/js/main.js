d41aa715251af5f4e1fc7f991a4df7eb53926f1b
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-26 03:21:24 -0500

    actualizacion de la  hora real

--- public/js/main.js
+++ public/js/main.js
@@ -1,64 +1,120 @@
-/*var socket;
-	var nombre;
-	var inputName;
-	window.onload = function(){
-		var lista = document.getElementById('lista');
-		inputName = document.getElementById('nombre');
-
-
-		socket = io.connect();
-
-		socket.on('connect', function () {
-			console.log("Cliente");
-			//socket.emit('identify', "Gerson3");
-		});
-
-		socket.on('historia', function(_mensajes)
-		{
-			for(var i in _mensajes)
-			{
-				lista.innerHTML += '<li>' + _mensajes[i] +  '</li>';
-			}
-		});
-
-		socket.on('saludo_global', function(_saludo){
-			lista.innerHTML += '<li>' + _saludo +  '</li>';
-		})
-	};
-
-	function enviar()
-	{
-		var input = document.getElementById('salida');
-		console.log(input.value);
-		socket.emit('saludo', inputName.value + ' :: ' + input.value);
-	}
-
-	function updateName()
-	{
-		socket.emit('identify', inputName.value);
-	}*/
-
-function chat()
+//hora actual del sistema
+var d = new Date();
+var horaActual=d.getHours()+':'+d.getMinutes();
+/****DEFINICION DE LAS CLASES CHAT, PERSON A.MESSAJE, WHATSSAP**/
+function Chat()
 {
-	this.people  = [];
+	this.nombre = '';
+	this.people = [];
 	this.messages = [];
-	this.chatAvatar ='';
+	this.chatAvatar = '';
 }
-function person(_name, _avatar)
+
+function Person(_name, _avatar)
 {
 	this.name = _name;
 	this.avatar = _avatar;
 }
-var tati = new  persona(tati,img);//eacion de un
-function whatsapp()
+function Message(_message, _sender)
+{
+	this.message = _message;
+	this.sender = _sender;
+	this.received = false;
+}
+function Whatsapp()
 {
 	this.chats = [];
-	this.activeChat = null;
-	this.searchChat = function(_keyword){
+	this.selectedChat = null;
+	this.searchChat		= function(_keyword){};
+	this.getChatFromId	= function(_chatId){};
+	this.drawChatList	= function(_htmlTarget){};
+	this.drawMessageList= function(){
+		var divChat = document.getElementById('chat');
+		divChat.innerHTML = '';
+
+		for (var i in this.selectedChat.messages) {
+			if (object.hasOwnProperty(i)) {
+				console.log(this.selectedChat.messages[i]);
+				this.sendMessage(this.selectedChat.messages[i], false);
+			}
+		}
 	};
-	this.getchChatFromId = function(_chatId){};
-	this.drawChatList = function(_htmlTarget){};
-	this.drawMessageList = function(_chatToDraw, htmlTarget){};
-	this.sendChat = function(_message){};
+	this.getLastMessage = function(){
+		return this.selectedChat.messages[this.selectedChat.messages.length-1];
+	};
+	this.sendMessage	= function(_message, _in){
+		//var htmlMessageIn = '<div class="w-message w-message-in"><div class="w-message-text"><p>' + _message.message + '</p><div class="time">14:27</div></div></div>';
+		//persona que mand a el mensaje
+		var htmlMessageOut = '<div class="w-message w-message-out"><div class="w-message-text"><p>' + _message.message + '</p><div class="time" >'+horaActual+'</div></div></div>';
+		var divChat = document.getElementById('chat');
+
+		this.selectedChat.messages.push(_message);
+
+		if(_in)
+		{
+			divChat.innerHTML += htmlMessageIn;
+		}else{
+			divChat.innerHTML += htmlMessageOut;
+		}
+
+		divChat.scrollTop = divChat.scrollHeight;
+	};
+}
+/****************INSTANCIACION DE OBJETOS*****************/
+
+//creamos las personas que mandaran mensajes
+var wapp = new Whatsapp();
+
+var tati = new Person('Tatiana');
+//var zare = new Person('Zare');
+
+var chat = new Chat();
+
+//chat.people.push(zare);
+
+wapp.chats.push(chat);
+wapp.selectedChat = chat;
+
+//wapp.sendMessage(new Message('Hola', zare));
+console.log(tati); //la persona que manda el mensaje
+
+//wapp.sendMessage(new Message('Que tal?', tati)); //para establecer ya mensajes
+
+//console.log(wapp.getLastMessage().sender);
+
+
+/****************LLAMADA A LA FUNCION CON E EVENTO KEYUP*************/
+window.onload = init;
+
+var inputMessage;
+var divChat;
+var chatPanel;
+
+function init()
+{
+	inputMessage = document.getElementById('mensajes');//inputs de mensajes
+	divChat = document.getElementById('chat');//contenedor de chats
+	var time = document.getElementsByClassName('time');
+	//chatPanel = document.querySelector('.w-chat-panel');
+	console.log(time);
+	inputMessage.addEventListener('keyup', onInputKeyUp);
 }
 
+function onInputKeyUp(evt)
+{
+	//console.log(evt.keyCode);
+	if(evt.keyCode == 13)
+	{
+		wapp.sendMessage(new Message(evt.target.value, tati));//manda mensajes con el obj tati d ela clase persona 
+		mensaje.innerHTML=inputMessage.value;
+		hora.innerHTML=horaActual;//id hoa de hora en html
+		evt.target.value = '';
+	}
+}
+
+
+/**/
+
+//funcion que seleciona en toda la lista de chat
+var chatsList=document.getElementsByClassName("listaChat");
+console.log(chatsList);//tengo todos los chats
